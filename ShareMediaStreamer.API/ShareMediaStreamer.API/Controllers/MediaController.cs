cfb83c48a5ec803a8c2b19f11be047e5326bc341
Abdul Bari <linked-blue@outlook.com>
2022-03-02 01:01:41 +0000

    Creates skeleton for media controller and adds get method

---
+++ ShareMediaStreamer.API/ShareMediaStreamer.API/Controllers/MediaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShareMediaStreamer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Add media controller actions here
+            return Ok();
+        }
+    }
+}
