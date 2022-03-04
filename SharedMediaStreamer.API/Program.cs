853c35ee5e4937c91498ed4ea6107b210231822f
Abdul Bari <linked-blue@outlook.com>
2022-03-04 04:07:45 +0000

    Update Program.cs
    Adds the media repository and file reader to the dependency container so it can then later be dependency injected into classes

--- SharedMediaStreamer.API/Program.cs
+++ SharedMediaStreamer.API/Program.cs
@@ -1,5 +1,12 @@
+using SharedMediaStreamer.Domain;
+using SharedMediaStreamer.Domain.Interfaces;
+using SharedMediaStreamer.MediaDataProcessor;
+using SharedMediaStreamer.MediaDataProcessor.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IMediaRepository, VideoRepository>();
+builder.Services.AddSingleton<IMediaFileReader, VideoFileReader>();
 
 // Add services to the container.
 
