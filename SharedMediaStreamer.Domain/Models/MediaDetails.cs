ee70593ad8d91dd16b78868266ade9e10d33cfd9
Abdul Bari <linked-blue@outlook.com>
2022-03-06 20:56:20 +0000

    Makes the media details model abstract so it can't be created directly but rather through its concrete implementation

--- SharedMediaStreamer.Domain/Models/MediaDetails.cs
+++ SharedMediaStreamer.Domain/Models/MediaDetails.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SharedMediaStreamer.Domain.Models
 {
-    public class MediaDetails
+    public abstract class MediaDetails
     {
         public long ContentLength { get; set; }
         public string ContentRange { get; set; }
