b0975e609b8f3c28511c5bbd323efb7981423a54
Abdul Bari <linked-blue@outlook.com>
2022-03-06 00:47:01 +0000

    Creates method to get media byte contents in the video file reader

--- SharedMediaStreamer.MediaDataProcessor/VideoFileReader.cs
+++ SharedMediaStreamer.MediaDataProcessor/VideoFileReader.cs
@@ -4,6 +4,12 @@ namespace SharedMediaStreamer.MediaDataProcessor
 {
     public class VideoFileReader : IMediaFileReader
     {
-
+        public void GetMediaByteContents(byte[] buffer, int offset, int length)
+        {
+            using (FileStream fileStream = File.Open(@"C:\Media\Test.mp4", FileMode.Open))
+            {
+                fileStream.Read(buffer, offset, length);
+            }
+        }
     }
 }
