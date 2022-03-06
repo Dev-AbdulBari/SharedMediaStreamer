e8efa23253d4d6bb282c44d98456c5d931e58c3e
Abdul Bari <linked-blue@outlook.com>
2022-03-06 20:54:13 +0000

    Creates base class media details model

---
+++ SharedMediaStreamer.Domain/Models/MediaDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedMediaStreamer.Domain.Models
+{
+    public class MediaDetails
+    {
+        public long ContentLength { get; set; }
+        public string ContentRange { get; set; }
+        public string ContentType { get; set; }
+    }
+}
