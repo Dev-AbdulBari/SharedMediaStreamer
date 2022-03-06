30be664d73da20bb1cd5ef4b1ddbddaf5dfac610
Abdul Bari <linked-blue@outlook.com>
2022-03-06 00:49:13 +0000

    Inverts dependency for a media file reader

--- SharedMediaStreamer.Domain/VideoRepository.cs
+++ SharedMediaStreamer.Domain/VideoRepository.cs
@@ -1,8 +1,14 @@
 ﻿using SharedMediaStreamer.Domain.Interfaces;
+using SharedMediaStreamer.MediaDataProcessor.Interfaces;
 
 namespace SharedMediaStreamer.Domain
 {
     public class VideoRepository : IMediaRepository
     {
+        private IMediaFileReader _mediaFileReader;
+        public VideoRepository(IMediaFileReader mediaFileReader)
+        {
+            _mediaFileReader = mediaFileReader;
+        }
     }
 }
