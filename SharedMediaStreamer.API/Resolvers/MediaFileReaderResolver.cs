965cf60ffaf2840cc6e365bcc1c6635c402684a6
Abdul Bari <linked-blue@outlook.com>
2022-03-09 03:40:21 +0000

    Creates media file reader resolver which implements the media file reader resolver interface
    Uses the service provider to get all the media file reader services in the service container and adds factory method which returns the file reader based on the type of the repository.

---
+++ SharedMediaStreamer.API/Resolvers/MediaFileReaderResolver.cs
@@ -0,0 +1,28 @@
+﻿using SharedMediaStreamer.Domain;
+using SharedMediaStreamer.Domain.Interfaces;
+using SharedMediaStreamer.MediaDataProcessor;
+
+namespace SharedMediaStreamer.API.Resolvers
+{
+    public class MediaFileReaderResolver : IMediaFileReaderResolver
+    {
+        private IServiceProvider _serviceProvider;
+        public MediaFileReaderResolver(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public IMediaFileReader GetMediaFileReader<T>()
+        {
+            return typeof(IMediaFileReader) switch
+            {
+                var givenType when typeof(T) == typeof(VideoRepository) => GetMediaFileReaderForRepository<T>(typeof(VideoFileReader)),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private IMediaFileReader GetMediaFileReaderForRepository<T>(Type mediaFileType)
+        {
+            return _serviceProvider.GetServices<IMediaFileReader>().Single(s => s.GetType() == mediaFileType);
+        }
+    }
+}
