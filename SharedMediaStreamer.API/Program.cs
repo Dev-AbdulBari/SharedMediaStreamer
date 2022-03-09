14402dd9d4c9014eb2b50b196a2e0081854efaa2
Abdul Bari <linked-blue@outlook.com>
2022-03-09 03:35:47 +0000

    Creates media settings model and implements options pattern in the service in program.cs

--- SharedMediaStreamer.API/Program.cs
+++ SharedMediaStreamer.API/Program.cs
@@ -1,11 +1,15 @@
+using SharedMediaStreamer.API.Resolvers;
 using SharedMediaStreamer.Domain;
 using SharedMediaStreamer.Domain.Interfaces;
+using SharedMediaStreamer.Domain.Models.Settings;
 using SharedMediaStreamer.MediaDataProcessor;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IMediaRepository, VideoRepository>();
 builder.Services.AddSingleton<IMediaFileReader, VideoFileReader>();
+
+builder.Services.Configure<MediaSettings>(builder.Configuration.GetSection("MediaSettings"));
 
 // Add services to the container.
 
---
+++ SharedMediaStreamer.Domain/Models/Settings/MediaSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedMediaStreamer.Domain.Models.Settings
+{
+    public class MediaSettings
+    {
+        public string FilePath { get; set; }
+        public short BufferSizeInMB { get; set; }
+    }
+}
