a9b497d980c7f67742230445e715a89d01e90cb2
Abdul Bari <linked-blue@outlook.com>
2022-06-09 20:59:17 +0100

    Adds dependency injection, allows any cors origin and uses chatrooms middleware

--- SharedMediaStreamer.API/Program.cs
+++ SharedMediaStreamer.API/Program.cs
@@ -1,3 +1,6 @@
+using SharedMediaStreamer.API.Middleware;
+using SharedMediaStreamer.API.Middleware.Interfaces;
+using SharedMediaStreamer.API.Middleware.Repository;
 using SharedMediaStreamer.API.Resolvers;
 using SharedMediaStreamer.Domain;
 using SharedMediaStreamer.Domain.Interfaces;
@@ -5,10 +8,20 @@ using SharedMediaStreamer.Domain.Models.Settings;
 using SharedMediaStreamer.MediaDataProcessor;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Allow-All",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyHeader();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IMediaRepository, VideoRepository>();
 builder.Services.AddSingleton<IMediaFileReader, VideoFileReader>();
 builder.Services.AddSingleton<IMediaFileReaderResolver, MediaFileReaderResolver>();
+builder.Services.AddSingleton<IRoomsRepository, RoomsRepository>();
 
 builder.Services.Configure<MediaSettings>(builder.Configuration.GetSection("MediaSettings"));
 
@@ -17,6 +30,12 @@ builder.Services.Configure<MediaSettings>(builder.Configuration.GetSection("Medi
 var app = builder.Build();
 
 app.UseRouting();
+
+app.UseCors("Allow-All");
+
+app.UseWebSockets();
+
+app.UseMiddleware<ChatroomsMiddleware>();
 
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
