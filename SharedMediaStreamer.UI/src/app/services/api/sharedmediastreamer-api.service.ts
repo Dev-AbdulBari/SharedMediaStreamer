95986fa8eb5a488b68ac0d665e286330a9c59c1a
Linked-Blue <Linked-Blue@outlook.com>
2022-11-26 19:15:05 +0000

    Creates shared media streamer api service to interact with api

--- SharedMediaStreamer.UI/src/app/services/api/sharedmediastreamer-api.service.ts
+++ SharedMediaStreamer.UI/src/app/services/api/sharedmediastreamer-api.service.ts
@@ -9,21 +9,30 @@ import { Room } from 'src/app/models/room.model';
 
 export class SharedMediaStreamerApiService {
   private httpClient: HttpClient;
+  private url: string = "<sharedMediaStreamerApiUrl>";
 
   constructor(httpClient: HttpClient) {
     this.httpClient = httpClient;
   }
 
   public getAllRooms(): Observable<Room> {
-    return this.httpClient.get<Room>("http://192.168.0.82:5000/api/room");
+    return this.httpClient.get<Room>(this.url + "/api/room");
   }
 
-  public getRoom(roomId: string) {
-    return this.httpClient.get<Room>("http://192.168.0.82:5000/api/room/" + roomId);
+  public getRoom(roomId: string): Observable<Room> {
+    return this.httpClient.get<Room>(this.url + "/api/room/" + roomId).pipe();
   }
 
   public postRoom(): Observable<string> {
-    return this.httpClient.post("http://192.168.0.82:5000/api/room", null, {responseType: 'text'}).pipe(catchError(error => {
+    debugger;
+    return this.httpClient.post(this.url + "/api/room", null, {responseType: 'text'}).pipe(catchError(error => {
+      console.log('Post request failed')
+      return '';
+    }))
+  }
+
+  public getSubtitles(): Observable<string> {
+    return this.httpClient.post(this.url + "/api/media/subtitles", null, {responseType: 'text'}).pipe(catchError(error => {
       console.log('Post request failed')
       return '';
     }))
