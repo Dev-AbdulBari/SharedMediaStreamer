cc66a15503d8790aa8331e8d47adb9b4aecece4d
Linked-Blue <Linked-Blue@outlook.com>
2022-11-26 04:43:01 +0000

    Adds CORS origin policy

--- SharedMediaStreamer.API/Program.cs
+++ SharedMediaStreamer.API/Program.cs
@@ -8,16 +8,9 @@ using SharedMediaStreamer.Domain.Models.Settings;
 using SharedMediaStreamer.MediaDataProcessor;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "Allow-All",
-        builder =>
-        {
-            builder.AllowAnyOrigin().AllowAnyHeader();
-        });
-});
 
 builder.Services.AddControllers();
+
 builder.Services.AddSingleton<IMediaRepository, VideoRepository>();
 builder.Services.AddSingleton<IMediaFileReader, VideoFileReader>();
 builder.Services.AddSingleton<IMediaFileReaderResolver, MediaFileReaderResolver>();
@@ -25,13 +18,21 @@ builder.Services.AddSingleton<IRoomsRepository, RoomsRepository>();
 
 builder.Services.Configure<MediaSettings>(builder.Configuration.GetSection("MediaSettings"));
 
+var corsPolicyOrigin = builder.Configuration.GetSection("CorsPolicyOrigins").Value;
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder => builder.WithOrigins(corsPolicyOrigin).AllowAnyHeader().AllowAnyMethod());
+});
+
 // Add services to the container.
 
 var app = builder.Build();
 
 app.UseRouting();
 
-app.UseCors("Allow-All");
+app.UseCors();
 
 app.UseWebSockets();
 
@@ -41,6 +42,5 @@ app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
 
 app.Run();
