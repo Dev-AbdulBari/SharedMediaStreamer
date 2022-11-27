07674acd8f6d5e908dd2d84d26971c5ff3c01060
Linked-Blue <Linked-Blue@outlook.com>
2022-11-27 03:05:48 +0000

    Adds user name to chat event message

--- SharedMediaStreamer.UI/src/app/models/chat-message-event.ts
+++ SharedMediaStreamer.UI/src/app/models/chat-message-event.ts
@@ -3,4 +3,5 @@ import { MessageEventType } from "../enums/message-event-type";
 export interface ChatMessageEvent {
     messageEventType: MessageEventType;
     message: String;
+    user: String;
 }
